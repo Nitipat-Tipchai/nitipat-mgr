c32b05cbcd91c6c6d59cf1d27183468db8e96d36
Nitipat-Tipchai <nitipat.ti@ku.th>
2026-06-25 10:41:45 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -362,7 +362,11 @@ async function startAppCore() {
     render();
     
     const urlParams = new URLSearchParams(window.location.search);
-    const checkinCourseId = urlParams.get('checkin');
+    let checkinCourseId = urlParams.get('checkin');
+    if (!checkinCourseId && window.location.hash.includes('checkin=')) {
+      checkinCourseId = window.location.hash.split('checkin=')[1].split('&')[0];
+    }
+    
     if (checkinCourseId) {
       setTimeout(() => {
         let foundCourse = null;
